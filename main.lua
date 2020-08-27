85e2a98e727b872d67644cf3954dcbb71a9bc849
marcusfelipe19 <marcus.19012005@gmail.com>
2020-08-27 19:55:36 -0300

    main.lua start

---
+++ main.lua
@@ -0,0 +1,84 @@
+push = require 'push'
+
+Class = require 'class'
+
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
+
+function love.load()
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+
+    love.window.setTitle('Space Invaders')
+
+    math.randomseed(os.time())
+
+    smallFont = love.graphics.newFont('font.ttf', 8)
+    scoreFont = love.graphics.newFont('font.ttf', 16)
+    love.graphics.setFont(smallFont)
+
+    --colocar som aqui depois.
+
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+        fullscreen = false,
+        resizable = true,
+        vsync = true,
+        canvas = false
+    })
+
+    --Colocar Jogador, bola e inimigos aqui.
+
+    score = 0
+
+    gameState = 'start'
+end
+
+function love.resize(w, h)
+    push:resize(w,h)
+end
+
+function love.update()
+
+end
+
+function love.keypressed(key)
+    if key == 'escape' then
+        love.event.quit()
+    elseif key == 'enter' or key == 'return' then
+        if gameState == 'start' then
+            gameState = 'play'
+        elseif gameState == 'done' then
+            gameState = 'play'
+
+            --Resetar tudo aqui.
+
+            score = 0
+        end
+    end
+end
+
+function love.draw()
+    push:start()
+
+    love.graphics.clear(40, 45, 52, 255)
+
+    if gameState == 'start' then
+        love.graphics.setFont(smallFont)
+        love.graphics.printf('Welcome to Space Invaders!', 0, 10, VIRTUAL_WIDTH, 'center')
+        love.graphics.printf('Press Enter to begin!', 0, 20, VIRTUAL_WIDTH, 'center')
+    end
+end
+
+function displayScore()
+    love.graphics.setFont(scoreFont)
+    love.graphics.print(tostring(score), 10, 15)
+end
+
+function displayFPS()
+    love.graphics.setFont(smallFont)
+    love.graphics.setColor(0, 255, 0, 255)
+    love.graphics.print('FPS: '.. tostring(love.timer.getFPS()), VIRTUAL_WIDTH - 20, 10)
+    love.graphics.setColor(255, 255, 255, 255)
+end
